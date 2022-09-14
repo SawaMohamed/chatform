6c193358ee646375506be5e2ebe7acb9f8f58ab3
Sawa Mohamed <88323793+SawaMohamed@users.noreply.github.com>
2022-09-14 11:34:31 +0200

    Delete connection.js

--- chatf-api/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://mohamed:dwoZvPzD0UCbs57G@test.4i6mk.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-  console.log('It has been connected');
-});
